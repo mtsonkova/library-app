38bfc704bf00a4bb9da12de78cc5d8ba9e9e3571
mtsonkova <mtsonkova950@gmail.com>
2026-02-04 22:22:36 +0200

    Create tailwind.config.ts file

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-    <Button>Click me</Button>
+      <Button>Click me</Button>
     </>
   );
 }
